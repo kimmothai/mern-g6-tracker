6f9adecbbc3e0c8a395ffde8a394ce66a77c33b3
kimmothai <kqmthai@gmail.com>
2020-08-25 01:19:01 +0300

    Added more API routes

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
 })
 
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercise', exercisesRouter)
+app.use('/users', usersRouter)
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
